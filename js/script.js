40ee6c6de40700e9224489e9587bd91f837bee6d
river-spring <rivermspring@gmail.com>
2020-07-09 12:53:50 +0900

    [Finish]JavaScript-12

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-$('#tab-contents .tab[id != "tab1"]').hide();
 
-$('#tab-menu a').on('click',function(event) {
-	$("#tab-contents .tab").hide();
-	$("#tab-menu .active").removeClass("active");
-	$(this).addClass("active");
-	$($(this).attr("href")).show();
-	event.preventDefault();
+$(function(){
+  $('#btn').on('click', function() {
+    // 入力された都市名でWebAPIに天気情報をリクエスト
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "7597f3027022d63d683164162ce582c8",
+      dataType : 'jsonp',
+    }).done(function (data){
+      //通信成功
+      // 位置
+      $('#place').text(data.name);
+      // 最高気温
+      $('#temp_max').text(data.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(data.main.temp_min);
+      //　湿度
+      $('#humidity').text(data.main.humidity);
+      //　風速
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function (data) {
+      //通信失敗
+　　　　alert('通信に失敗しました。');      });
+  });
 });
